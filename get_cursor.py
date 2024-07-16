0c1c36b4321ec8538268ad2d88432dfaab4393ce
AHADGH001 <99137282+AHADGH001@users.noreply.github.com>
2024-07-16 23:44:00 +0530

    Add files via upload

---
+++ get_cursor.py
@@ -0,0 +1,8 @@
+import pyautogui    #pyautogui tells us the position of cursor
+ 
+while True:
+   a= pyautogui.position()
+   print(a)
+   #1388,1060
+   #1396,614
+   #1603,958
---
+++ openai.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+import openai
+ 
+# pip install openai 
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(api_key = "your key")
+
+command="chat history"
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a person named harry who speaks hindi as well as english. He is from India and is a coder. You analyze chat history and respond like Harry"},
+    {"role": "user", "content": command}
+  ]
+)
+
+print(completion.choices[0].message.content)
